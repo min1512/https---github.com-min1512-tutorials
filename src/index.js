1e884a94ee4aa51b610e2cc4551ffb203f64b71f
min1512 <min1512@hanmail.net>
2022-06-07 00:39:43 +0900

    ReducerTutorial 공부

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReducerTutorial from './ReducerTutorial';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ReducerTutorial />
   </React.StrictMode>
 );
 
